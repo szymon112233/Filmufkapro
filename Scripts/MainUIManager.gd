f7c49bb194af19a36149997a01143e98af099ea4
szymon112233 <szymmon123@gmail.com>
2022-11-12 18:27:00 +0100

    Added labels to choices

--- Scripts/MainUIManager.gd
+++ Scripts/MainUIManager.gd
@@ -10,10 +10,14 @@ func SetupNewQuestion(question: Question):
 	questionLabel.text = question.QuestionContent
 	
 	var option1Image = get_node(PathToOption1).get_node("ColorRect/TextureRect") as TextureRect
+	var option1Label = get_node(PathToOption1).get_node("ColorRect/Label") as Label
 	option1Image.texture = question.Choice1.Icon
+	option1Label.text = question.Choice1.Name
 	
 	var option2Image = get_node(PathToOption2).get_node("ColorRect/TextureRect") as TextureRect
+	var option2Label = get_node(PathToOption2).get_node("ColorRect/Label") as Label
 	option2Image.texture = question.Choice2.Icon
+	option2Label.text = question.Choice2.Name
 	
 	get_node(PathToQuestionLabel).hide()
 	get_node(PathToOption1).hide()
