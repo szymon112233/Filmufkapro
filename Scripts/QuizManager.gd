c2ee9965f35c22777ce4c612fce7135fa848c143
szymon112233 <szymmon123@gmail.com>
2022-11-12 13:03:15 +0100

    Added basic choice gameplay

--- Scripts/QuizManager.gd
+++ Scripts/QuizManager.gd
@@ -1,14 +1,47 @@
 extends Node
 class_name QuizManager
 
-export(Array, Resource) var Questions
+export(Array, Resource) var AllQuestions
+export(Array, Resource) var TutorialQuestionsQueue
 
 export(NodePath) var PathToMainUI
 
-func _ready():
-	var mainUIManager = get_node(PathToMainUI).get_node("MainUIManager") as MainUIManager
-	mainUIManager.SetupNewQuestion(Questions[0] as Question)
+var CurrentQuestionIndex
+var CurrentQuestion: Question
+var mainUIManager
 
+func _ready():
+	mainUIManager = get_node(PathToMainUI).get_node("MainUIManager") as MainUIManager
+	CurrentQuestionIndex = -1;
+	NextTutorialQuestion()
+
+func _input(event):
+	if event.is_action_released("left_option"):
+		MakeChoice(true)
+	elif event.is_action_released("right_option"):
+		MakeChoice(false)
+		
+func NextTutorialQuestion():
+	if CurrentQuestionIndex >= TutorialQuestionsQueue.size() -1 :
+		Finish()
+		return
+	CurrentQuestionIndex += 1
+	CurrentQuestion = (TutorialQuestionsQueue[CurrentQuestionIndex] as Question)
+	mainUIManager.SetupNewQuestion(CurrentQuestion)
+
+func MakeChoice(isLeft: bool):
+	if isLeft:
+		Anwser(CurrentQuestion.Choice1Valid)
+	else:
+		Anwser(CurrentQuestion.Choice2Valid)
+		
+		
+func Anwser(isCorrect: bool):
+	print(isCorrect)
+	NextTutorialQuestion()
+	
+func Finish():
+	print("Koniec")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
