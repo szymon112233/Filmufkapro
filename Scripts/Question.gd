ccaa08bf82b1355a1b445dcfb6287538340122ec
szymon112233 <szymmon123@gmail.com>
2022-11-12 12:17:09 +0100

    Added choice validity to question.

--- Scripts/Question.gd
+++ Scripts/Question.gd
@@ -2,5 +2,7 @@ extends Resource
 class_name Question
 
 export(String) var QuestionContent
+export(bool) var Choice1Valid
+export(bool) var Choice2Valid
 export(Resource) var Choice1
 export(Resource) var Choice2
