468d9ce38eec593458860648ef9fdf10a4060db1
szymon112233 <szymmon123@gmail.com>
2022-11-13 01:04:52 +0100

    Cleanup

--- Scripts/AudioManager.gd
+++ Scripts/AudioManager.gd
@@ -5,13 +5,7 @@ class_name AudioManager
 
 func _ready():
 	$"../AnimationPlayer".play("MainBeat")
-	
-func PlayBeat(currentSpeedMultiplier: float):
-	$AudioStreamPlayer.pitch_scale = currentSpeedMultiplier 
-	$AudioStreamPlayer.play()
-
 
 func _on_QuizManager_NewQuestionShown(currentSpeedMultiplier):
-	# PlayBeat(currentSpeedMultiplier)
 	$"../AnimationPlayer".playback_speed = currentSpeedMultiplier
 	$"../AudioStreamPlayer".pitch_scale = currentSpeedMultiplier
